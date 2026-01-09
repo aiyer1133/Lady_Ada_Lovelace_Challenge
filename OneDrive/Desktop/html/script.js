bdac8c718b621fb00ea9d9dd2fd0988f1c6a199a
aiyer1133 <ananya.iyer.7613@gmail.com>
2026-01-09 16:38:11 +0530

    Delete OneDrive/Desktop/html directory

--- OneDrive/Desktop/html/script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("btn");
-const text = document.getElementById("text");
-
-button.addEventListener("click", () => {
-  text.textContent = "DOM manipulated 😎 JavaScript works!";
-});
